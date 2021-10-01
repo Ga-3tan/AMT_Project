f2176977ccdf5bc95e3463de0937751c0c73754a
Alessandro-AP <alessandro.parrinoap@gmail.com>
2021-10-01 12:51:31 +0200

    test : testing ci

---
+++ src/test/java/com/example/serverweb/ApplicationTest.java
@@ -0,0 +1,27 @@
+package com.example.serverweb;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ApplicationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void homepageShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("lorem Ipsum");
+    }
+}
