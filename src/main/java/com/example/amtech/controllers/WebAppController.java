33a85f5394e1ca979db6229a4300394cc2d2edbb
DS-Daniel <daniel.sciarra@heig-vd.ch>
2021-11-15 10:07:33 +0100

    remove autowire + add dockerVolume to gitignore

--- src/main/java/com/example/amtech/controllers/WebAppController.java
+++ src/main/java/com/example/amtech/controllers/WebAppController.java
@@ -3,7 +3,6 @@ package com.example.amtech.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebAppController {
